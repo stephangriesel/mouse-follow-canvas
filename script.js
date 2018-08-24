6d929bb04cde810747831b37773644018e901247
Stephan Griesel <sgriesel@gmail.com>
2018-08-24 10:33:44 +0200

    start js

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
-console.log(123);
+var $ = document.querySelector.bind(document);
+var $on = document.addEventListener.bind(document);
+
+var xmouse, ymouse;
+$on('mousemove', function (e) {
+    xmouse = e.clientX || e.pageX;
+    ymouse = e.clientY || e.pageY;
+});
+
+var ball = $('#ball');
+var x = void 0,
+    y = void 0,
+    dx = void 0,
+    dy = void 0,
+    tx = 0,
+    ty = 0,
+    key = -1
+
+
+
+
