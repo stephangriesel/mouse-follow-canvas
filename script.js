706bbd249659ec15c1257d0953abc2a917325b1e
Stephan Griesel <sgriesel@gmail.com>
2018-08-24 10:43:38 +0200

    js done

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ $on('mousemove', function (e) {
     ymouse = e.clientY || e.pageY;
 });
 
-var ball = $('#ball');
+var ball = $('#canvas');
 var x = void 0,
     y = void 0,
     dx = void 0,
@@ -16,6 +16,25 @@ var x = void 0,
     ty = 0,
     key = -1
 
+var followMouse = function followMouse() {
+    key = requestAnimationFrame(followMouse);
 
+    if(!x || !y) {
+        x = xmouse;
+        y = ymouse;
+    } else {
+        dx = (xmouse - x) * 0.125;
+        dy = (ymouse - y) * 0.125;
+        if(Math.abs(dx) + Math.abs(dy) < 0.1) {
+            x = xmouse;
+            y = ymouse;
+        } else {
+            x += dx;
+            y += dy;
+        }
+    }
+    ball.style.left = x + 'px';
+    ball.style.top = y + 'px';
+};
 
 
